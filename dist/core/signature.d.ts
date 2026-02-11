8977189cd2c4e37c68cc5ec560deeda29829a98d
John <admin@MacBook-Pro-admin.local>
2026-02-11 10:47:38 +0500

    chore: remove node_modules from repo

--- dist/core/signature.d.ts
+++
@@ -1,9 +0,0 @@
-import type { HashAlgorithm } from '../interfaces/common.types';
-/**
- * Sign an XML string with an RSA private key in DER format (PKCS#1).
- *
- * Replicates PHP:
- *   cat xml | openssl dgst -<alg> -binary -keyform DER -sign secret.key | openssl base64 -A
- */
-export declare function signXml(xml: string, derPrivateKey: Buffer, algorithm: HashAlgorithm): string;
-//# sourceMappingURL=signature.d.ts.map
--- dist/payout.constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const PAYOUT_MODULE_OPTIONS: unique symbol;
-export declare const PRODUCTION_URL = "https://lk.payin-payout.net/api_out/";
-export declare const SANDBOX_URL = "https://dev1.payin-payout.net/";
-//# sourceMappingURL=payout.constants.d.ts.map
